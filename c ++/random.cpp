2b510c6d4a9470e5c78117ab23ed99a73c659d7c
liblissz <liblissz3@gmail.com>
2025-01-05 16:36:37 -0800

    Add files via upload

---
+++ c ++/random.cpp
@@ -0,0 +1,14 @@
+#include <iostream > 
+using namespace std;
+                           int main(){
+                               int age;
+                               char grade;
+                               float gpa;
+                               bool isStudent;
+
+                             std::  cout << age ;
+                               cout << grade ;
+                               cout << gpa ;
+                               cout << isStudent ;
+                               return 0;
+                           } 
