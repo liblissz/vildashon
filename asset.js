1f74d23f7fe6df8881da86a0d6cf5340dc32aee5
liblissz <liblissz3@gmail.com>
2025-01-05 18:11:06 -0800

    Add files via upload

--- asset.js
+++ asset.js
@@ -1,16 +1,17 @@
 const pages = [
-    {title: "csharp(c#)", url:"csharp.html", content:"learn c#" },
-    {title: "feedback", url:"feedback.html", content:"give a feedback message" },
-    {title: "java", url:"java.html", content:"learn java" },
-    {title: "python", url:"python.html", content:"learn python" },
-    {title: "c program", url:"c.html", content:"learn c" },
-    {title: "c++", url:"cplusplus.html", content:"learn c++" },
-    {title: "javscript js", url:"java.html", content:"learn javascript(js)" },
-    {title: "css", url:"css.html", content:"learn casceding stylesheet(css)" },
-    {title: "html", url:"html.html", content:"learn html" },
-    {title: "terms and condition", url:"term.html", content:"term and condition" },
-    {title: "use AI", url:"ai.html", content:"use  AI" },
-    {title: "c++", url:"cplusplus.html", content:"learn c++" }
+    {title: "csharp(c#)", url:"../csharp.html", content:"learn c#" },
+    {title: "feedback", url:"../feedback.html", content:"give a feedback message" },
+    {title: "java", url:"../java.html", content:"learn java" },
+    {title: "python", url:"../python.html", content:"learn python" },
+    {title: "c program", url:"../c.html", content:"learn c" },
+    {title: "c++", url:"../cplusplus.html", content:"learn c++" },
+    {title: "javscript js", url:"../java.html", content:"learn javascript(js)" },
+    {title: "css", url:"../css.html", content:"learn casceding stylesheet(css)" },
+    {title: "html", url:"../html.html", content:"learn html" },
+    {title: "terms and condition", url:"../term.html", content:"term and condition" },
+    {title: "use AI", url:"../ai.html", content:"use  AI" },
+    {title: "c++", url:"../cplusplus.html", content:"learn c++" },
+    {title: "homepage", url:"../homepage.html", content:"visit our home page" }
     
     ];
     
@@ -30,7 +31,7 @@ const pages = [
         resultDiv.style.padding = "10px";
         resultDiv.style.borderRadius = "10px";
             resultDiv.innerHTML = `<h2><a style ="color: white;" href="${result.url}">${result.title}</a></h2>
-            <p>${result.content}</p>`;
+            <p style = "color:white">${result.content}</p>`;
             resultcontainer.appendChild(resultDiv);
             
         });
